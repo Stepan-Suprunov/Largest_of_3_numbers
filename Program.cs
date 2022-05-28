1db0c063ec418f1a77273a6297835f4faa42de39
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-05-28 18:48:27 +0300

    Sorting numbers to the right

--- Program.cs
+++ Program.cs
@@ -4,3 +4,22 @@ Console.WriteLine("Введите число B");
 int NumB = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число C");
 int NumC = int.Parse(Console.ReadLine());
+//Сортировка чисел
+int Mem = NumA;
+if (NumA>NumB){
+    Mem = NumB;
+    NumB = NumA;
+    NumA = Mem;
+}
+if (NumB>NumC){
+    Mem = NumC;
+    NumC = NumB;
+    NumB = Mem;
+}
+//Мониторинг
+Console.WriteLine("___");
+Console.WriteLine(NumA);
+Console.WriteLine(NumB);
+Console.WriteLine(NumC);
+Console.WriteLine("___");
+Console.WriteLine(Mem);
