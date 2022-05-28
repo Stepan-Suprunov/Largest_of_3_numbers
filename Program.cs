0ef4f9a7704b04c870c614069e496ca6cfbb61b5
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-05-28 18:15:44 +0300

    Request insert numbers

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.WriteLine("Введите число А");
+int NumA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int NumB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int NumC = int.Parse(Console.ReadLine());
